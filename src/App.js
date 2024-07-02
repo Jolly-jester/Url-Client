226c93a4252778882cdd1a0ffeddff90481ab46b
aditya singh <adityasingh@adityas-MacBook-Air.local>
2024-07-02 16:36:17 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState(''); // State to store the shortened URL
+
+  async function submit(ev) {
+    ev.preventDefault();
+    console.log({ Url });
+
+    try {
+      const response = await fetch('http://localhost:4000/', {
+        method: 'POST',
+        body: JSON.stringify({ Url }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      setShortUrl(`http://localhost:4000/${data.short}`); // Update state with the shortened URL
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>fakeUrl</h1>
+        <input
+          type="text"
+          placeholder="Paste Your Url"
+          value={Url}
+          onChange={(ev) => {
+            setUrl(ev.target.value);
+          }}
+        />
+        <button onClick={submit}>Shorten URL</button>
+
+        {shortUrl && (
+          <div>
+            <h2>Shortened URL:</h2>
+            <a href={shortUrl} target="_blank" rel="noopener noreferrer">
+              {shortUrl}
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
